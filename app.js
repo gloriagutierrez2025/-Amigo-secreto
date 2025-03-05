829ca1ab33a6b05dc16c350235f1a9a07cb16b41
gloriagutierrez2025 <gloriagutierrezdelaparra@gmail.com>
2025-03-05 10:32:45 -0300

    crea el index.html

--- app.js
+++ app.js
@@ -1,1 +1,39 @@
 console.log("ejecutando el sistema");
+let amigo=[];
+function agregarAmigo (){
+    let inputAmigo=document.getElementById("amigo");
+    let nombreAmigo=inputAmigo.value; 
+    if (!nombreAmigo){
+        alert("Debes ingresar un nombre");
+        return;
+    }
+amigo.push(nombreAmigo);
+console.log(amigo);
+inputAmigo.value="";
+nombreAmigo.value="";
+inputAmigo.focus();
+renderizarAmigos();
+}
+function renderizarAmigos(){
+
+    let listaAmigos=document.getElementById("listaAmigos");
+    listaAmigos.innerHTML = "";
+    for(let i=0; i<amigo.length;i++){
+let item=document.createElement("li");
+item.textContent=amigo[i];
+listaAmigos.appendChild(item);
+    }
+}
+function sortearAmigo(){
+    if(amigo.length===0){
+        alert("no hay amigos para sortear");
+        return;
+    }
+
+let amigoSorteado=amigo[parseInt(Math.random()*amigo.length )];
+console.log("valor: " + amigoSorteado);
+let resultado=document.getElementById ("resultado");
+resultado.innerHTML=(`El amigo sorteado es: ${amigoSorteado}`);
+let limpiarLista=document.getElementById("listaAmigos");
+limpiarLista.innerHTML=""; 
+}
